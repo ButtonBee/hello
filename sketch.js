48c5490c343ab667db0aa19380685b8527305444
ButtonBee <clazhaz.productions@gmail.com>
2026-09-22 16:36:14 +0100

    removing "/" in font file line

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ let font;
 async function setup() {                  //happens once when it first runs
     createCanvas(800, 600);
     car = new AutomaticCar();       // speed, gear, rpm
-    font = await loadFont('/assets/ds-digi.ttf');
+    font = await loadFont('assets/ds-digi.ttf');
     textFont(font);
 }
 
